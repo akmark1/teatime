ef9cd1a39fa30d23bdf4fbb0fa818e195b1049db
akmark1 <akmark123@gmail.com>
2021-03-26 18:58:02 +0900

    投稿機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :url
+    validates :image
+  end
 end

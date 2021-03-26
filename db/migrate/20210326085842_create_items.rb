17ae7d7df90e4280376fc29ae4e8a823a9b663bc
akmark1 <akmark123@gmail.com>
2021-03-26 18:06:43 +0900

    itemsテーブルの作成

---
+++ db/migrate/20210326085842_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string     :name,        null:false
+      t.text       :description, null:false
+      t.text       :url,         null:false
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end

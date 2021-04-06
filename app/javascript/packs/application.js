1ef5a3bedaccb95a726d9d2a811f302ef7b29728
akmark1 <akmark123@gmail.com>
2021-04-06 17:05:26 +0900

    サインインページのビューの調整

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,9 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
-require('../jquery-3.6.0.min')
 require('../menu')
 require('../vegas.min')
 require('../slider')
